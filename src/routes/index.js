0ea1b6c78dfd2100009c90f59f325a3781250d46
AlanS2405 <alan.dario.solis@gmail.com>
2023-10-08 23:45:09 -0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { sendEmail } = require("../utils/sendEmail");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Welcome to express");
+});
+
+router.post("/emails/contact", (req, res) => {
+  const { name, email, phone, message } = req.body;
+
+  sendEmail({
+    to: `alan.dario.solis@gmail.com`,
+    subject: "Portfolio Email",
+    text: `Name: ${name}
+Email: ${email}
+
+${message}`,
+  });
+
+  res.send({ message: "Your message is send :D" }).status(200);
+});
+
+module.exports = router;
